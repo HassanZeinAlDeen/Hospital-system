582b707fdac0125bcf80692ee0a01782b97c8309
HassanZeinAlDeen <hassanzeinaldeen0@gmail.com>
2023-03-11 13:37:21 +0200

    adding display medication api

--- back-end/displaymedications.php
+++ back-end/displaymedications.php
@@ -1,5 +1,14 @@
 <?php
 
+include('connection.php');
 
+$query = $mysql -> prepare('SELECT Name from Medications');
+$query->execute();
+$result = $query->get_result();
 
+while($object = $result->fetch_assoc()){
+  $data[] = $object;
+}
+$response["medications"]=$data;
+echo json_encode($response);
 ?>
